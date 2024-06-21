2000a37f89329ae3af5ce52f42465723da9dd929
amrewly <ameliaismania@gmail.com>
2024-06-21 12:24:01 +0700

    cc

--- services/authServices.js
+++ services/authServices.js
@@ -4,7 +4,8 @@ const generateToken = (user) => {
     const payload = {
         user: {
             id: user.id,
-            name: user.name
+            name: user.name,
+            email: user.email
         }
     };
     return jwt.sign(payload, "s3cr3t_g4m1ng_123", { expiresIn: '1h' });
